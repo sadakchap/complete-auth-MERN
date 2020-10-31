1f28c4b06b237b02a8605ff01b531ccaffe935ac
sadakchap <aliceprerna@gmail.com>
2020-11-01 03:15:16 +0530

    User ResetPassword Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import AdminRoute from './AdminRoute';
 import AdminDashboard from './pages/AdminDashboard';
 import ConfirmEmail from './pages/ConfirmEmail';
 import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import UserDashboard from './pages/UserDashboard';
 import PrivateRoute from './PrivateRoute';
 const { default: Authenticate } = require("./pages/Authenticate");
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" component={Authenticate} exact />
         <Route path="/user/verify/:token" component={ConfirmEmail} exact />
         <Route path="/user/password/forgot" component={ForgotPassword} />
+        <Route path="/user/password/reset/:token" component={ResetPassword} />
         <PrivateRoute path="/user/dashboard" component={UserDashboard} exact />
         <AdminRoute path="/admin/dashboard" component={AdminDashboard} exact />
       </Switch>
